c7b537738dfdfb51d14a51ed8f097408cd7f6ffe
crystal123456 <313733041@qq.com>
2015-08-13 17:49:51 +0800

    验证码和logindao

--- src/cn/pleasantdesign/dao/LoginDao.java
+++ src/cn/pleasantdesign/dao/LoginDao.java
@@ -1,15 +1,18 @@
 package cn.pleasantdesign.dao;
 
+import com.vanceinfo.framework.orm.hibernate.HibernateBaseDao;
+
 import cn.pleasantdesign.entity.User;
 
-public class LoginDao {
+public class LoginDao extends HibernateBaseDao<User, Long> {
 	
 	public boolean userLogin(String userName, String password){
 		return true;
 	}
 	
 	public User validateUserName(String userName){
-		return null;
+		User user =findUniqueBy("loginName", userName);
+		return user;
 	}
 
 }
--- src/cn/pleasantdesign/entity/User.java
+++ src/cn/pleasantdesign/entity/User.java
@@ -2,12 +2,22 @@ package cn.pleasantdesign.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
  * 用户类
  * 
  * @author fred
  *
  */
+@Entity
+@Table(name="pleasant_user")
 public class User {
 
 	// 用户id
@@ -28,6 +38,10 @@ public class User {
 	// 登录时间
 	private Date loginTime;
 
+	@Id 
+    @Column(name="ID", unique=true, precision=22, scale=0)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="TKMS_MEMBERS_SEQ")      
+    @SequenceGenerator(name="TKMS_MEMBERS_SEQ",allocationSize=1,initialValue=100, sequenceName="TKMS_MEMBERS_SEQ")
 	public Long getId() {
 		return id;
 	}
@@ -36,6 +50,7 @@ public class User {
 		this.id = id;
 	}
 
+	@Column(name="USER_NAME", nullable=false, length=100)
 	public String getUserName() {
 		return userName;
 	}
@@ -44,6 +59,7 @@ public class User {
 		this.userName = userName;
 	}
 
+	@Column(name="PASSWORD", nullable=false, length=50)
 	public String getPassword() {
 		return password;
 	}
